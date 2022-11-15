a6c9ef7837993e723090787afd0e680456fb6f62
Bayu Candra <110009488+Bayu-x3@users.noreply.github.com>
2022-11-15 07:49:24 +0700

    Update praktek1.js

--- praktek1.js
+++ praktek1.js
@@ -1,4 +1,4 @@
-// Program menginput dan memunculkan Nama awal dan Nama Akhir
+// Program menginput dan memunculkan Nama awal dan Nama Akhir. Menggunakan readline-sync
 
 var readlineSync = require('readline-sync'); // menambahkan package readline-sync
 var frs = readlineSync.question('First name: '); // menginput nama awal
